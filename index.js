5eb7091936ae0d5dbddb263a84efaf4735469a90
Wildtyto <wildtyto@gmail.com>
2019-04-17 11:35:14 +0800

    Update read-line

--- index.js
+++ index.js
@@ -3,19 +3,28 @@
     const __readline = require("readline");
     const __utils = require("./utils");
 
+    const _programDictionary = {
+            1: {
+                    name: "Base Converter",
+                    action: _doBaseConvert
+                }
+        };
+    const _colorProgram = "\x1b[44m%s\x1b[0m";
+    const _colorWrong = "\x1b[33m%s\x1b[0m";
+
     ( function () {
 
-        const _programDictionary = {
-                1: {
-                        name: "Base Converter",
-                        action: _doBaseConvert
-                    }
-            };
-        const _programList = Object.keys(_programDictionary);
+        _askProgram();
+
+    }() );
+
+    function _askProgram() {
+
+        const _programList = Object.keys(_programDictionary) ;
         const _readline = __readline.createInterface({
                 input: process.stdin,
                 output: process.stdout
-            });
+            }) ;
         console.log("Which program do you want to process?");
         _programList.map( _key => console.log(`[ ${_key} ] ${ (_programDictionary[_key] || {} ).name }`) );
         _readline.question("> ", _answer => {
@@ -24,20 +33,29 @@
                     const _key = _programList.shift();
                     const _data = _programDictionary[_key] || {} ;
                     if ( _answer !== _key && _answer !== _data.name ) continue;
-                    console.log("\x1b[44m%s\x1b[0m", `Running program: ${ _data.name }`);
+                    console.log(_colorProgram, `Running program: ${ _data.name }`);
                     _data.action();
                     return;
                 }
-                console.log("\x1b[33m%s\x1b[0m", "Can not find the program!");
+                console.log(_colorWrong, "Can not find the program!");
+                _askProgram();
             });
 
-    }() );
+    }
 
     function _doBaseConvert() {
 
-        console.log(__utils.baseConverter(100345, 2));
-        console.log(__utils.baseConverter(100345, 8));
-        console.log(__utils.baseConverter(100345, 16));
+        const _readline = __readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+        _readline.question("Number: ", _requestNumber => {
+                _readline.question("Base (2): ", _baseNumber => {
+                        _readline.close();
+                        console.log(__utils.baseConverter(_requestNumber, ( _baseNumber || 2 )));
+                        _askProgram();
+                    } );
+            } );
 
     }
 
